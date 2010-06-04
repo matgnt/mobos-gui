fb04df2b923ebb6d58e8165a3b4bb8b981ab9ea3
Matthias Günther <matgnt@gmail.com>
2010-06-05 01:12:15 +0200

    main: Load dummydata.

--- src/main.cpp
+++ src/main.cpp
@@ -10,6 +10,7 @@
 
 #include <QtDeclarative/QDeclarativeContext>
 #include <QtDeclarative/QDeclarativeView>
+#include <QtDeclarative/QDeclarativeComponent>
 
 int main(int argc, char *argv[])
 {
@@ -18,7 +19,29 @@ int main(int argc, char *argv[])
     qmlRegisterType<Ofono>("OfonoLib", 1, 0, "Ofono");
 
     QDeclarativeView view;
+
     view.rootContext()->setContextProperty("OfonoContext", new Ofono());
+
+    QFile f(":qml/dummydata/Recipes.qml");
+    f.open(QIODevice::ReadOnly);
+    QByteArray data = f.readAll();
+    QDeclarativeComponent comp(view.engine());
+    comp.setData(data, QUrl());
+    QObject *dummyData = comp.create();
+
+    if(comp.isError()) {
+        QList<QDeclarativeError> errors = comp.errors();
+        foreach (const QDeclarativeError &error, errors) {
+            qWarning() << error;
+        }
+    }
+
+    if (dummyData) {
+        qWarning() << "Loaded dummy data.";
+        view.rootContext()->setContextProperty("Recipes", dummyData);
+        //dummyData->setParent(this);
+    }
+
     view.setSource(QUrl::fromLocalFile(":qml/mobos-gui.qml"));
     view.show();
 
